54e69e0a1e6c63483607c8de9afda6c5211417a0
Селбаев Алмас <almasselbayev@gmail.com>
2022-03-15 23:32:03 +0600

    UITabBar + Model

---
+++ HomeWork 2.3.1/ViewControllers/UserViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UserViewController.swift
+//  HomeWork 2.3.1
+//
+//  Created by Almas Selbayev on 15.03.2022.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+    
+    var userName: String!
+    var user = User()
+
+    @IBOutlet var fullNameTextField: UITextField!
+    @IBOutlet var ageTextField: UITextField!
+
+    @IBOutlet var jobTitleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = userName
+        
+        fullNameTextField.text = user.person.fullname
+        ageTextField.text = String(user.person.age)
+        jobTitleTextField.text = user.person.job.title
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let moreInfoController = segue.destination as? MoreInfoViewController
+        else { return }
+        
+        moreInfoController.user = user
+    }
+    
+
+
+}
